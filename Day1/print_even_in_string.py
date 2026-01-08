bd5a2980ed568e787de7158a7be5b998543242c9
MONKMAESTER <92608138+W4R-L0RD@users.noreply.github.com>
2026-01-08 21:30:33 +0530

    print even solve

--- Day1/print_even_in_string.py
+++ Day1/print_even_in_string.py
@@ -1,8 +1,11 @@
 '''
 Exercise 3: Print characters present at an even index number
-Write a Python code to accept a string from the user and display characters present at an even index number.
+Write a Python code to accept a string from the user 
+and 
+display characters present 
+at an even index number.
 
-For example, str = "PYnative". so your code should display ‘P’, ‘n’, ‘t’, ‘v’.
+For example, str = "PYnative". so your code should display P, n, t, v.
 
 Expected Output:
 
@@ -13,3 +16,38 @@ n
 t
 v
 '''
+stringinput = input("enter string")
+
+
+#  find one more way to solve this. 
+
+
+
+
+
+
+
+index = 0
+while index < len(stringinput):
+ print(stringinput[index])
+ index+=2
+
+for index in range(0,len(stringinput),2):
+ print(stringinput[index])
+
+
+x = len(stringinput)
+for index in range(x):
+ if index %2 == 0:
+  print(stringinput[index])
+
+index =0
+while index < len(stringinput):
+ if index %2 == 0:
+  print(stringinput[index])
+ index+=1
+
+
+
+
+print("\n".join([stringinput[i] for i in range(len(stringinput)) if i%2 ==0]))
