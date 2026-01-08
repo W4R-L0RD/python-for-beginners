996086189d4b548805e8be1641d7886aadb6c642
Bhola Kumar <bhola@Bholas-MacBook-Air.local>
2026-01-08 20:54:08 +0530

    t

--- Day1/sum_of_cur_prev.py
+++ Day1/sum_of_cur_prev.py
@@ -2,3 +2,10 @@
 Exercise 2: Print the Sum of a Current Number and a Previous number
 l : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,12,12,12,12,12]
 '''
+
+for i in range (10):
+    if i == 0: print(0)
+    else:
+        print (i + i-1)
+    
+    
